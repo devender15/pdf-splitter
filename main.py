f2771c1d93fe4e5b6697ca50838c6b4779e53fe2
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-04-05 23:58:24 +0530

    improved code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
+import PyPDF2
 from PyPDF2 import PdfWriter, PdfReader
 import os
 import shutil
@@ -28,6 +29,13 @@ pdfs = [pdf for pdf in os.listdir(INPUT_DIR + "/") if pdf.endswith(".pdf")]
 PDF_COUNT = 0
 FAILED_PDF_COUNT = 0
 
+# Define the page sizes
+page_sizes = {
+    "A3": (1191, 842),
+    "A4": (842, 595),
+    "A5": (595, 421),
+}
+
 rprint("[bold red]Script started...[/bold red]")
 
 for pdf in pdfs:
@@ -48,7 +56,8 @@ for pdf in pdfs:
     num_pages = len(pdf_reader.pages)
 
     # putting the original file in the output folder
-    shutil.copy(INPUT_DIR + "/" + f"{pdf}", os.path.join(output_dir, "original.pdf"))
+    shutil.copy(INPUT_DIR + "/" + f"{pdf}",
+                os.path.join(output_dir, "original.pdf"))
     tree.add(f"[sandy_brown]Original PDF ✅[/sandy_brown]")
     PDF_COUNT += 1
 
@@ -57,7 +66,7 @@ for pdf in pdfs:
     for i in range(1, 3):
         combinations = list(itertools.combinations(range(num_pages), i))
         page_combinations.extend(combinations)
-    
+
     # Generate 2 PDF files with 1 page each for an input PDF file with 2 pages
     if num_pages == 2:
         # Split into combinations of 1
@@ -65,18 +74,31 @@ for pdf in pdfs:
             try:
                 pdf_writer = PdfWriter()
                 page = pdf_reader.pages[page_num]
-                pdf_writer.add_page(page)
-                output_filename = f"{pdf_name}_set-1_variant-{i+1}.pdf"
-                output_path = os.path.join(output_dir, output_filename)
-                with open(output_path, 'wb') as output_file:
-                    pdf_writer.write(output_file)
-                PDF_COUNT += 1
-                # rprint(f"[green][{pdf_name}] Generated {output_filename}[/green]")
-                tree.add(f"Set of 1 variant - {page_num+1} ✅")
+
+                for size_name, size in page_sizes.items():
+                    # Create a new page object with the correct size
+                    new_page = PyPDF2.pdf.PageObject.createBlankPage(
+                        size[0], size[1])
+                    new_page.mergeScaledTranslatedPage(page, scaleToFit=True)
+                    pdf_writer.add_page(new_page)
+                    output_filename = f"{pdf_name}_set-1_variant-{i+1}_{size_name}.pdf"
+                    output_path = os.path.join(output_dir, output_filename)
+                    with open(output_path, 'wb') as output_file:
+                        pdf_writer.write(output_file)
+                    PDF_COUNT += 1
+                    tree.add(f"Set of 1 variant - {page_num+1} ✅")
+
+                # pdf_writer.add_page(page)
+                # output_filename = f"{pdf_name}_set-1_variant-{i+1}.pdf"
+                # output_path = os.path.join(output_dir, output_filename)
+                # with open(output_path, 'wb') as output_file:
+                #     pdf_writer.write(output_file)
+                # PDF_COUNT += 1
+                # tree.add(f"Set of 1 variant - {page_num+1} ✅")
 
             except:
                 FAILED_PDF_COUNT += 1
-                tree.add(f"Set of 1 variant - {page_num+1} ❌")    
+                tree.add(f"Set of 1 variant - {page_num+1} ❌")
 
     # Generate 3 PDF files with 1 page each for an input PDF file with 3 pages
     if num_pages == 3:
@@ -96,7 +118,7 @@ for pdf in pdfs:
                 PDF_COUNT += 1
                 tree.add(f"Set of 2 variant - {i+1} ✅")
                 # rprint(f"[green][{pdf_name}] Generated {output_filename}[/green]")
-            
+
             except Exception as e:
                 FAILED_PDF_COUNT += 1
                 # rprint("[bold red]Error:[/bold red] ", e, f" - {pdf_name} (Set of 2 variant - {page_num+1}")
@@ -115,11 +137,10 @@ for pdf in pdfs:
                     pdf_writer.write(output_file)
                 PDF_COUNT += 1
                 tree.add(f"Set of 1 variant - {page_num+1} ✅")
-            
+
             except:
                 FAILED_PDF_COUNT += 1
                 tree.add(f"Set of 1 variant - {page_num+1} ❌")
-
 
     # Generate all possible combinations of sets of 3 pages for an input PDF file with 6 pages
     if num_pages == 6:
@@ -147,11 +168,10 @@ for pdf in pdfs:
 
                     PDF_COUNT += 1
                     tree.add(f"Set of {page_set} variant - {i+1} ✅")
-                
+
                 except:
                     FAILED_PDF_COUNT += 1
                     tree.add(f"Set of {page_set} variant - {i+1} ❌")
-
 
     # showing the tree
     rprint(tree)
@@ -160,10 +180,15 @@ for pdf in pdfs:
     pdf_file.close()
 
 # showing the summary of the output
-rprint("-------------------------[bold red]Summary of the output-------------------------")
+rprint(
+    "-------------------------[bold red]Summary of the output-------------------------")
 rprint(f"[dark_olive_green2]Input directory: {INPUT_DIR}[/dark_olive_green2]")
-rprint(f"[dark_olive_green2]Output directory: {OUTPUT_DIR}[/dark_olive_green2]")
+rprint(
+    f"[dark_olive_green2]Output directory: {OUTPUT_DIR}[/dark_olive_green2]")
 rprint(f"[dark_olive_green2]Total input PDFs: {len(pdfs)}[/dark_olive_green2]")
-rprint(f"[dark_olive_green2]Total PDFs in output: {len(os.listdir(OUTPUT_DIR))}[/dark_olive_green2]")
-rprint(f"[dark_olive_green2]Total PDFs generated: {PDF_COUNT}[/dark_olive_green2]")
-rprint(f"[dark_olive_green2]Total PDFs failed: {FAILED_PDF_COUNT}[/dark_olive_green2]")
+rprint(
+    f"[dark_olive_green2]Total PDFs in output: {len(os.listdir(OUTPUT_DIR))}[/dark_olive_green2]")
+rprint(
+    f"[dark_olive_green2]Total PDFs generated: {PDF_COUNT}[/dark_olive_green2]")
+rprint(
+    f"[dark_olive_green2]Total PDFs failed: {FAILED_PDF_COUNT}[/dark_olive_green2]")
